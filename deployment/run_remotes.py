21bdd39ff7b0df78a7c55c83984e2dc09b28e69a
James <Jiawu1998@github.com>
2021-04-13 16:59:31 -0500

    changes to run_remotes.py

--- deployment/run_remotes.py
+++ deployment/run_remotes.py
@@ -1,54 +1,51 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import subprocess
 import shlex
 import argparse
+import os
 
+
+base_directory = os.path.dirname(os.path.abspath(__file__))
 
 def valid_ip(ip):
     return len(ip.split(".")) == 4
 
-def get_ips(file_name, only_data=False):
-    """
-    assume that the file line will be:
-        <id_val>: <ip_addr> or
-        <ip_addr> <domain>.local <domain>
-    """
-    
-    global found_ips
-    found_ips = 0
-    def run_ip(ip, is_data=True):
-        valid = valid_ip(ip)
-        global found_ips
-        if valid: found_ips += 1 # not great
-        return valid and (not only_data or is_data)
+def get_ips(file_name):
 
     ips = []
     with open(file_name, 'r') as f:
         for line in f.readlines():
-            vals = line.split()
-            if len(vals) == 2:
-                key = ''.join(filter(str.isalnum, vals[0]))
-                addr = vals[1]
-                if run_ip(addr, key.isnumeric()): 
-                    ips.append(addr)
-
-            elif len(vals) == 3: #alt format from /etc/hosts
-                addr = vals[0]
-                if run_ip(addr, found_ips>=1): #very first one is namenode
-                    ips.append(addr)
+            if valid_ip(line):
+                ips.append(line)
     return ips
 
+def redis_scp(ips, user, slave_count, sentinal_count):
+    if len(ips) >= 1 + slave_count + sentinal_count:
+        for i in range(len(ips)):
+            if i == 0:
+                subprocess.run(["scp", f"{base_directory}/redis/install.sh", f"{base_directory}/redis/start.py", f"{base_directory}/redis/confs/master.conf", f"{user}@{ips[i]}/home"])
+            elif i <= slave_count:
+                subprocess.run(["scp", f"{base_directory}/redis/install.sh", f"{base_directory}/redis/start.py", f"{base_directory}/redis/confs/slave.conf", f"{user}@{ips[i]}/home"])
+            elif i <= slave_count + sentinal_count:
+                subprocess.run(["scp", f"{base_directory}/redis/install.sh", f"{base_directory}/redis/start.py", f"{base_directory}/redis/confs/sentinel.conf", f"{user}@{ips[i]}/home"])
+        
+    else:
+        print("not enough instances")
 
-def run_ips(ips, user, in_cmd, write_out=None):
+def mongodb_scp(ips, user):
+    for i in range(len(ips)):
+        subprocess.run(["scp", f"{base_directory}/mongodb/install.sh", f"{base_directory}/mongodb/start.py", f"{base_directory}/mongodb/cluster.conf", f"{user}@{ips[i]}/home"])
+
+def run_ips(ips, user, cmd, write_out=None):
     ssh_stdout = []
-    for ip in ips:
-        cmd = shlex.split("ssh {}@{} \"{}\"".format(user, ip, in_cmd))
+    for ip in ips.keys():
+        cmd = shlex.split(f'ssh {user}@{ip} "{cmd}"')
         ran = subprocess.run(cmd, stdout=subprocess.PIPE, check=True, universal_newlines=True)
         if write_out:
             ssh_stdout.append(ran.stdout)
         else:
-            print("finished cmd {} at {} with output:\n{}".format(in_cmd, ip, ran.stdout)) 
+            print(f"finished cmd {cmd} at {ip} with output:\n{ran.stdout}") 
 
     if write_out:
         with open(write_out, "a+") as f:
@@ -56,19 +53,32 @@ def run_ips(ips, user, in_cmd, write_out=None):
 
 if __name__ == "__main__":
     parse = argparse.ArgumentParser("Runs the command on all ssh ips in the supplied file")
-    parse.add_argument("-c", "--cmd",  default='ls', help="command to run in each ip, cannot include double quote (\") characters")
-    parse.add_argument("-d", "--data", action="store_true", help="apply to only data ips")
+    parse.add_argument("-d", "--database",  default='redis', help="select database (redis or mongodb)")
     parse.add_argument("-f", "--file", help="file that contains the ips")
     parse.add_argument("-o", "--out",  help="write output of ssh stdout to file")
-    parse.add_argument("-s", "--ssh",  help="single target to run the command on, has precedence over the file arg")
     parse.add_argument("-u", "--user", default='ubuntu', help="the user for the ips, for now all the same")
     args = parse.parse_args()
 
-    if not args.ssh:
-        ips = get_ips(args.file, args.data)
-    elif valid_ip(args.ssh):
-        ips = [args.ssh]
-    else:
-        ips = []
+    ips = get_ips(args.file)
 
-    if ips != []: run_ips(ips, args.user, args.cmd, args.out)
+    #note: fixed number of slaves and sentials for redis and fixed number of slaves for mongodb
+    if args.database == "redis":
+        redis_scp(ips, args.user, 1, 1)
+
+    elif args.database == "mongodb":
+        mongodb_scp(ips, args.user)
+
+    if ips != []: 
+        cmd0 = "cd ~/home"
+        run_ips(ips, args.user, cmd0, args.out)
+
+        cmd1 = "./install.sh"
+        run_ips(ips, args.user, cmd1, args.out)
+
+        if args.database == "redis":
+            cmd2 = "./start.py"
+
+        elif args.database == "mongodb":
+            cmd2 = "./start.py"
+        
+        run_ips(ips, args.user, cmd2, args.out)
