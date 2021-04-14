390bb55b14b66bf38b24bd52cecafa3615c69ff9
James <Jiawu1998@github.com>
2021-04-13 21:34:00 -0500

    changes to run remote.py

--- deployment/run_remotes.py
+++ deployment/run_remotes.py
@@ -37,9 +37,43 @@ def mongodb_scp(ips, user):
     for i in range(len(ips)):
         subprocess.run(["scp", f"{base_directory}/mongodb/install.sh", f"{base_directory}/mongodb/start.py", f"{base_directory}/mongodb/cluster.conf", f"{user}@{ips[i]}/home"])
 
+def redis_start(ips, user):
+    master_node_port = 6379
+    for i in range(len(ips)):
+        if i == 0:
+            conf = "master.sentinel"
+            other = ""
+        elif i == 1:
+            conf = "sentinel.conf"
+            other = f"-s -m {ips[0]} -p {master_node_port}"
+        else:
+            conf = "slave.conf"
+            other = f"-m {ips[0]} -p {master_node_port}"
+
+        cmd = f"./start.py -r /usr/local/bin/redis-server -c {conf} {other}"
+        cmd = " ".split(f'ssh {user}@{ips[i]} "{cmd}"')
+        ran = subprocess.run(cmd, stdout=subprocess.PIPE, check=True, universal_newlines=True)
+
+        print(f"finished cmd {cmd} at {ips[i]} with output:\n{ran.stdout}") 
+
+def mongo_start(ips, user):
+    for i in range(len(ips)):
+        if i == 0:
+            role = "mongos"
+        elif i == 1:
+            role = "config"
+        else:
+            role = "shards"
+
+        cmd = f"./start.py -c cluster.json -m {i} -r {role}"
+        cmd = " ".split(f'ssh {user}@{ips[i]} "{cmd}"')
+        ran = subprocess.run(cmd, stdout=subprocess.PIPE, check=True, universal_newlines=True)
+
+        print(f"finished cmd {cmd} at {ips[i]} with output:\n{ran.stdout}") 
+
 def run_ips(ips, user, cmd, write_out=None):
     ssh_stdout = []
-    for ip in ips.keys():
+    for ip in ips:
         cmd = shlex.split(f'ssh {user}@{ip} "{cmd}"')
         ran = subprocess.run(cmd, stdout=subprocess.PIPE, check=True, universal_newlines=True)
         if write_out:
@@ -61,14 +95,15 @@ if __name__ == "__main__":
 
     ips = get_ips(args.file)
 
-    #note: fixed number of slaves and sentials for redis and fixed number of slaves for mongodb
-    if args.database == "redis":
-        redis_scp(ips, args.user, 1, 1)
+    if ips != []:
 
-    elif args.database == "mongodb":
-        mongodb_scp(ips, args.user)
+        #note: fixed number of slaves and sentials for redis and fixed number of slaves for mongodb
+        if args.database == "redis":
+            redis_scp(ips, args.user, 1, 1)
 
-    if ips != []: 
+        elif args.database == "mongodb":
+            mongodb_scp(ips, args.user)
+    
         cmd0 = "cd ~/home"
         run_ips(ips, args.user, cmd0, args.out)
 
@@ -76,12 +111,11 @@ if __name__ == "__main__":
         run_ips(ips, args.user, cmd1, args.out)
 
         if args.database == "redis":
-            cmd2 = "./start.py"
-
-        elif args.database == "mongodb":
-            cmd2 = "./start.py -c cluster.json "
+            redis_start(ips, args.user)
         
-        run_ips(ips, args.user, cmd2, args.out)
+        elif args.database == "mongodb":
+            mongo_start(ips, args.user)
+            
 
 
 
