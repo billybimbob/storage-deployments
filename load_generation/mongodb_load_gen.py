0ecbb89acc2e492b0aee17441d9e7bfd7c9785ce
James <Jiawu1998@github.com>
2021-04-19 14:02:04 -0500

    changes to random string generation length and create directory path if not created

--- load_generation/mongodb_load_gen.py
+++ load_generation/mongodb_load_gen.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+
 import os
 import random
 import string
 
 output_path = f"{os.path.dirname(os.path.abspath(__file__))}/load_output/mongodb"
 
+STRING_LEN = 100
 letters = string.ascii_lowercase
 keys = []
 
@@ -11,9 +14,9 @@ def generate_random_string(length):
     return ''.join(random.choice(letters) for _ in range(length))
 
 def add_write_operations(file):
-    key = generate_random_string(10)
+    key = generate_random_string(STRING_LEN)
     keys.append(key)
-    file.write(f'WRITE {key} "{generate_random_string(10)}" \n')
+    file.write(f'WRITE {key} "{generate_random_string(STRING_LEN)}" \n')
 
 def add_read_operations(file): 
     if len(keys) != 0:
@@ -28,6 +31,9 @@ if __name__ == "__main__":
     load_types = ["write", "read", "metadata"]
     load_sizes = [1000, 10000, 100000]
 
+    if not os.path.isdir(output_path):
+        os.makedirs(output_path)
+
     for t in load_types:
         for load in load_sizes:
             file = open(f"{output_path}/{t}_{load}_opearations.csv", 'w')
@@ -41,7 +47,6 @@ if __name__ == "__main__":
                 if t == "meta":
                     add_meta_operations(file)
 
-            keys = []
             file.flush()
             file.close()
 
--- load_generation/redis_load_gen.py
+++ load_generation/redis_load_gen.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from io import TextIOWrapper
 import os
 import random
@@ -5,6 +7,7 @@ import string
 
 output_path = f"{os.path.dirname(os.path.abspath(__file__))}/load_output/redis"
 
+STRING_LEN = 100
 letters = string.ascii_lowercase
 keys = []
 
@@ -12,22 +15,25 @@ def generate_random_string(length:int):
     return ''.join(random.choice(letters) for _ in range(length))
 
 def add_write_operations(file:TextIOWrapper):
-    key = generate_random_string(10)
+    key = generate_random_string(STRING_LEN)
     keys.append(key)
-    file.write(f'SET {key} "{generate_random_string(10)}" \n')
+    file.write(f'SET {key} "{generate_random_string(STRING_LEN)}" /n')
 
 def add_read_operations(file:TextIOWrapper): 
     if len(keys) != 0:
-        file.write(f"GET {random.choice(keys)}\n")
+        file.write(f"GET {random.choice(keys)}/n")
     else: 
         raise RuntimeError("ERROR: No keys to GET")
 
 def add_meta_operations(file:TextIOWrapper):
-    file.write(f'SET myhash {hash} "{generate_random_string(10)}"')
+    file.write(f'SET myhash {hash} "{generate_random_string(STRING_LEN)}"')
 
 if __name__ == "__main__":
     load_types = ["write", "read", "metadata"]
     load_sizes = [1000, 10000, 100000]
+
+    if not os.path.isdir(output_path):
+        os.makedirs(output_path)
 
     for t in load_types:
         for load in load_sizes:
@@ -42,7 +48,6 @@ if __name__ == "__main__":
                 if t == "meta":
                     add_meta_operations(file)
 
-            keys = []
             file.flush()
             file.close()
 
