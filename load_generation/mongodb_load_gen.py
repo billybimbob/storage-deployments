d85b5a98e96cf18dd520e07237c9d0348399dd96
James <Jiawu1998@gmail.com>
2021-04-20 00:17:00 -0500

    changed loadgen file for mongodb

--- load_generation/mongodb_load_gen.py
+++ load_generation/mongodb_load_gen.py
@@ -15,43 +15,34 @@ output_path = f"{os.path.dirname(os.path.abspath(__file__))}/load-output/mongodb
 
 STRING_LEN = 100
 letters = string.ascii_lowercase
-keys: List[str] = []
-
+key = "key"
 
 def generate_random_string(length: int):
     return ''.join(random.choice(letters) for _ in range(length))
 
+def add_write_operations(operations: List[str]):
+    val = generate_random_string(STRING_LEN)
+    operations.append({'insert':[key, val]})
 
-def add_write_operations(file: TextIOWrapper):
-    key = generate_random_string(STRING_LEN)
-    keys.append(key)
-    file.write(f'INSERT {key} "{generate_random_string(STRING_LEN)}" \n')
+def add_read_operations(operations: List[str]):  
+    operations.append({'find':[key]})
 
-
-def add_read_operations(file: TextIOWrapper): 
-    if not keys:
-        raise RuntimeError("ERROR: No keys to GET")
-
-    file.write(f"FIND {random.choice(keys)}\n")
-
-
-def add_meta_operations(file: TextIOWrapper): 
-    file.write(f"CREATECOLLECTION {generate_random_string(STRING_LEN)}\n")
-
+def add_meta_operations(operations: List[str]): 
+    operations.append({'createCollection':[generate_random_string(STRING_LEN)]})
 
 def create_operations(op: Operation, load: int):
     with open(f"{output_path}/{op}_{load}_operations.txt", 'w') as f:
-        for _ in range(load):
+        operations = []
+        for _ in range(load): 
             if op == "write":
-                add_write_operations(f)
+                add_write_operations(operations)
                 
             elif op == "read":
-                add_read_operations(f)
+                add_read_operations(operations)
 
             elif op == "meta":
-                add_meta_operations(f)
-
-
+                add_meta_operations(operations)
+        f.write(str(operations))
 
 if __name__ == "__main__":
     ops: List[Operation] = ['write', 'read', 'meta']
