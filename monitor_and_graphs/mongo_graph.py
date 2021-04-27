4eb6596ae1b8d86e355d76ec2412191160f7e047
billybimbob <noahluvsmilkduds@gmail.com>
2021-04-27 07:29:30 +0000

    mongo graphing

--- monitor_and_graphs/mongo_graph.py
+++ monitor_and_graphs/mongo_graph.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
-from typing import Dict, List, TypedDict, Union
+from __future__ import annotations
+from collections import defaultdict
+from typing import (
+    Dict, List, Literal, NamedTuple, Optional, Tuple, TypedDict, Union, cast)
+
 from argparse import ArgumentParser
 from datetime import datetime
 from pathlib import Path
 
 import json
-import matplotlib as plt
+import re
+import matplotlib.pyplot as plt
 
 
 class Work(TypedDict):
@@ -15,11 +20,15 @@ class Work(TypedDict):
     count: int
     "number of operations"
 
+WorkKey = Literal['time', 'count']
+
 
 class Poll(TypedDict):
     total: Work
     read: Work
     write: Work
+
+PollKey = Literal['total', 'read', 'write']
 
 
 class Timestamp(TypedDict):
@@ -31,10 +40,42 @@ class Timestamp(TypedDict):
 class Runtime(TypedDict):
     start: str
     end: str
-    rops: float
+    ops: float
 
 
-INTERVAL = 2
+class RunParams(NamedTuple):
+    name: str
+    value: str
+    op: str
+    size: int
+
+    @staticmethod
+    def file_key(file: Union[str, Path]):
+        if isinstance(file, str):
+            file = Path(file)
+
+        return f'{file.parent.name}: {file.stem}'
+
+
+    @classmethod
+    def from_key(cls, key: str) -> Optional[RunParams]:
+        match = re.match(r'mongo-([^:]+): top-([\D]+)(\d+)', key)
+        if not match:
+            return None
+
+        split = re.match(r'[^-]+', match[1][::-1])
+        if not split:
+            return None
+        
+        split = split.end()
+        return cls(
+            name = match[1][:-split-1],
+            value = match[1][-split:],
+            op = match[2],
+            size = int(match[3]) )
+
+
+GRAPHS = Path('mongo-graphs')
 
 DB = "test-db.test-col1"
 DB_CACHE = "config.cache.chunks.test-db.test-col1"
@@ -42,7 +83,7 @@ DB_CACHE = "config.cache.chunks.test-db.test-col1"
 FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
 
-def parse_times(mongo_top: List[Timestamp]) -> Dict[datetime, Poll]:
+def get_polls(mongo_top: List[Timestamp]) -> Dict[datetime, Poll]:
     stamps: Dict[datetime, Poll] = {}
 
     for stamp in mongo_top:
@@ -55,7 +96,11 @@ def parse_times(mongo_top: List[Timestamp]) -> Dict[datetime, Poll]:
 
         def add_poll(info: Poll):
             for poll_val in poll:
+                poll_val = cast(PollKey, poll_val)
+
                 for work_val in poll[poll_val]:
+                    work_val = cast(WorkKey, work_val)
+
                     info_val = info[poll_val][work_val]
                     poll[poll_val][work_val] += info_val
 
@@ -72,69 +117,204 @@ def parse_times(mongo_top: List[Timestamp]) -> Dict[datetime, Poll]:
 
 
 
-def parse_files(file: Union[str, Path]) -> Dict[str, Runtime]:
+def get_runtimes(file: Union[str, Path]) -> Optional[Dict[str, Runtime]]:
     if isinstance(file, str):
         file = Path(file)
 
     if file.is_dir():
         data: Dict[str, Runtime] = {}
         for f in file.iterdir():
-            data = {**data, **parse_files(f)}
+            new_data = get_runtimes(f)
+
+            if new_data:
+                data.update(new_data)
 
         return data
 
     if not file.is_file() or file.suffix != '.json':
-        return {}
+        return None
 
     with open(file) as f:
         json_data = json.load(f)
 
-    times = parse_times(json_data)
+    polls = get_polls(json_data)
 
-    if not times:
-        return {}
+    if not polls:
+        return None
 
-    avgs = [poll['total'] for poll in times.values()]
+    totals = [ poll['total'] for poll in polls.values() ]
     avgs = [
-        avg['count'] / avg['time'] * 1000 # might seem a bit high
-        for avg in avgs
-        if avg['time'] > 0]
+        total['count'] / total['time'] * 1000
+        for total in totals
+        if total['time'] > 0 ]
 
-    rops = sum(avgs) / len(avgs) if avgs else 0
-
-    start = min(times.keys())
-    end = max(times.keys())
+    start = min(polls.keys())
+    end = max(polls.keys())
+    ops = sum(avgs) / len(avgs) if avgs else 0
 
     run = Runtime(
         start = str(start),
         end = str(end),
-        rops = rops)
-    
-    run_name = f'{file.parent.name}: {file.stem}'
-    return {run_name: run}
+        ops = ops)
 
-    # start = f'{start.minute} minutes {start.second:02d} seconds'
-    # end = f'{end.minute} minutes {end.second:02d} seconds'
-
-    # print(f'{file.name}: {start=} {end=} {a.seconds} secs')
+    return { RunParams.file_key(file): run }
 
 
 
-def write_parse(times: Dict[str, Runtime], outfile: Union[str, Path]):
+def write_runtimes(times: Dict[str, Runtime], outfile: Union[str, Path]):
     with open(outfile, 'w') as f:
         json.dump(times, f, indent=4)
+
+
+
+def graph_runtimes(runtimes: Union[str, Path, Dict[str, Runtime]]):
+    if not isinstance(runtimes, dict):
+        with open(runtimes) as f:
+            runtimes = cast(Dict[str, Runtime], json.load(f))
+
+    run_params = split_by_names(runtimes)
+
+    for name, vals in run_params.items():
+        graph_by_size(name, vals)
+        graph_by_value(name, vals)
+
+
+
+def split_by_names(runtimes: Dict[str, Runtime]):
+    run_params: Dict[str, List[Tuple[RunParams, float]]]
+    run_params = defaultdict(list)
+
+    for run_key, run_val in runtimes.items():
+        param = RunParams.from_key(run_key)
+        if param is None:
+            raise RuntimeError('could not parse key')
+
+        run_params[param.name].append((param, run_val['ops']))
+
+    return run_params
+
+
+def split_by_param_value(values: List[Tuple[RunParams, float]]):
+    value_split: Dict[str, List[Tuple[str, int, float]]]
+    value_split = defaultdict(list)
+
+    for param, ops in values:
+        value_split[param.value].append((param.op, param.size, ops))
+
+    return value_split
+
+
+
+def graph_by_size(name: str, values: List[Tuple[RunParams, float]]):
+    graph_params = split_by_param_value(values)
+
+    plt.figure(figsize=(8,6), dpi=80, facecolor='w', edgecolor='k')
+
+    for param, results in graph_params.items():
+        read_vals = [r for r in results if r[0] == 'read']
+        read_vals = sorted(read_vals, key=lambda t: t[1])
+
+        x_vals = list(range(len(read_vals)))
+
+        plt.plot(
+            x_vals,
+            [v[2] for v in read_vals],
+            label = 'reads')
+
+        write_vals = [w for w in results if w[0] == 'write']
+        write_vals = sorted(write_vals)
+
+        plt.plot(
+            x_vals,
+            [v[2] for v in write_vals],
+            label = 'writes')
+
+        plt.legend()
+        plt.xticks(x_vals, [v[1] for v in read_vals])
+
+        plt.xlabel('Number of Operations')
+        plt.ylabel('Requests Per Second')
+        plt.title(f'{name}: {param}')
+
+        plt.savefig(f'{GRAPHS}/{name}-{param}')
+        plt.clf()
+
+
+
+def graph_by_value(name: str, values: List[Tuple[RunParams, float]]):
+    value_names = split_by_param_value(values)
+    graph_params = cast(Dict[str, Tuple[float, float]], {})
+
+    for param, results in value_names.items():
+        read_ops = [r[2] for r in results if r[0] == 'read' and r[1] > 0]
+        if read_ops:
+            read_ops = sum(read_ops) / len(read_ops)
+        else:
+            read_ops = 0
+
+        write_ops = [w[2] for w in results if w[0] == 'write' and w[1] > 0]
+        if write_ops:
+            write_ops = sum(write_ops) / len(write_ops)
+        else:
+            write_ops = 0
+
+        # read then write ops
+        graph_params[param] = read_ops, write_ops
+
+    def str_cmp(string: str):
+        if string.isdigit():
+            return int(string)
+        else:
+            return string
+
+    graph_params = sorted(graph_params.items(), key=lambda t: str_cmp(t[0]))
+    # width = 1.4 / (len(graph_params) * 1.5)
+    width = 0.3
+    slots = range(len(graph_params))
+
+    plt.figure(figsize=(8,6), dpi=80, facecolor='w', edgecolor='k')
+
+    plt.bar(
+        [s - width/2 for s in slots],
+        [r for _, (r, _) in graph_params],
+        width,
+        label = 'reads')
+
+    plt.bar(
+        [s + width/2 for s in slots],
+        [w for _, (_, w) in graph_params],
+        width,
+        label = 'writes')
+
+    plt.xticks(list(slots), [v for v, _ in graph_params])
+    plt.legend()
+
+    plt.xlabel(f'{name} Values')
+    plt.ylabel('Requests Per Second')
+    plt.title(f'{name} Parameter Values')
+
+    plt.savefig(f'{GRAPHS}/{name}-parameters')
+
 
 
 if __name__ == '__main__':
     args = ArgumentParser(description='parse mongotop files')
 
     args.add_argument('-d', '--directory',
-        required = True,
         help = 'directory of mongotop json files')
-    args.add_argument('-o', '--outfile',
-        default = 'out.json',
-        help = 'file to write parsed info to')
+
+    args.add_argument('-r', '--runfile',
+        default = 'run.json',
+        help = 'file to write runtimes to')
 
     args = args.parse_args()
-    data = parse_files(args.directory)
-    write_parse(data, args.outfile)
+
+
+    GRAPHS.mkdir(exist_ok=True, parents=True)
+
+    # runtimes = get_runtimes(args.directory)
+    # if runtimes:
+    #     write_runtimes(runtimes, args.runfile)
+
+    # if runtimes:
+    graph_runtimes(args.runfile)
